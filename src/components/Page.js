e28937b27ba0a05c853fda9d0e5dde8cd039478c
Siarhei Vaitehovich <swarog2002@mail.ru>
2017-12-18 20:33:17 +0300

    master. Add async action

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 
 export default class Page extends Component {
   onYearBtnClick(e) {
-    this.props.setYear(parseInt(e.target.textContent))
+    this.props.getPhotos(parseInt(e.target.textContent))
   }
 
   render() {
-    const { year, photos } = this.props
+    const { year, photos, fetching } = this.props
 
     return <div className="ib page">
       <p>
@@ -16,7 +16,11 @@ export default class Page extends Component {
         <button onClick={this.onYearBtnClick.bind(this)} className="btn">2014</button>
       </p>
       <h3>{year} год</h3>
-      <p>У тебя {photos.length} фото</p>
+      {
+        fetching
+        ? <p>Загрузка...</p>
+        : <p>У тебя {photos.length} фото.</p>
+      }
     </div>
   }
 }
@@ -24,5 +28,5 @@ export default class Page extends Component {
 Page.propTypes = {
   year: PropTypes.number.isRequired,
   photos: PropTypes.array.isRequired,
-  setYear: PropTypes.func.isRequired
+  getPhotos: PropTypes.func.isRequired
 }
